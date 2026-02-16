6e864640aaafd0bf7f8db7bc50aa4d08924fb5af
Bohdan Kasianchuk <bogdan.k1990@gmail.com>
2026-02-16 18:10:10 +0200

    Update global-error.tsx

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,9 +1,13 @@
 'use client';
 
+import { Plus_Jakarta_Sans } from 'next/font/google';
+
+const font = Plus_Jakarta_Sans({ subsets: [ 'latin' ] });
+
 export default function Error() {
     return (
         <html lang="en">
-            <body>
+            <body className={ font.className }>
                 <main>
                     <div className="flex items-center justify-center w-full px-5">
                         <p className="text-5xl text-primary font-bold leading-[1.2] text-center">
